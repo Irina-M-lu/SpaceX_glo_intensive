9af2a4ba8d1bba950f353806d563e1175ce5bb96
Irina-M-lu <61713113+Irina-M-lu@users.noreply.github.com>
2020-10-17 13:04:53 +0200

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Main from '../Main/Main';
+import useLaunches from '../useLaunches/useLaunches';
+import Youtube from 'react-youtube';
+import './details.css'
+
+const Details = (props) => {
+
+const [launch, setLaunch] = useState(null);
+
+const { getLaunch } = useLaunches();
+
+useEffect(() => {
+  setLaunch(getLaunch(props.match.params.id));
+})
+
+console.log(launch);
+  const history = useHistory(null);
+
+  if(!launch) return <div>Loading</div>
+  return (
+  <>
+  <Main name={launch.name}/>
+  <main className="details">
+    <div className="container">
+      <div className="details-row">
+        <div className="details-image">
+          <img src={launch.links.patch.small} alt=""/>
+        </div>
+        <div className="details-content">
+          <p className="details-description">{launch?.details}</p>
+        </div>
+</div>
+<Youtube className='details-youtube' videoId={launch.links.youtube_id}/>
+    </div>
+    <a onClick={history.goBack} className="button button-back">go back</a>
+  </main>
+</>
+)};
+
+export default Details;
